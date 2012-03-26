14309a4981b4a749734f6fad1b9afbce4ed44ac1
Jens-Andre Koch <jakoch@web.de>
2012-03-26 22:16:08 +0200

    - added jQ checkbox (replaces checkboxes with an on/off image toggle)

--- webskins/znc-ation/pub/js/znc.js
+++ webskins/znc-ation/pub/js/znc.js
@@ -3,8 +3,11 @@
  */
 $(document).ready(function()
 {
-    /* binds the tooltip to input formelements
-       uses "title" attr as source for tooltip text */
+    /**
+     * jQ.Poshytip
+     * This binds the tooltip to input formelements
+     * and uses "title" attr as source for tooltip text.
+     */
     $('input[title]').poshytip({
         className: 'tip-yellowsimple',
         showOn: 'focus',
@@ -13,4 +16,10 @@ $(document).ready(function()
         offsetX: 0,
         offsetY: 5
     });
+    /**
+     * jQ.Checkbox
+     * Replace input formelements of type checkbox with an image toggle.
+     */
+    $('input:checkbox)').checkbox();
+    $('input:radio').checkbox();
 })
